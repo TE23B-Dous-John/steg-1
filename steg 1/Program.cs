88f72d08fa5807eebbc0dc02d121c5655eff7106
John Dous <john.dous@elev.ga.ntig.se>
2024-11-08 14:44:19 +0100

    first commit

---
+++ steg 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Net.Security;
+
+if ( 6 >= 3)
+{
+    Console.WriteLine("hello World");
+}
+
+Console.WriteLine("Write your username");
+string username = Console.ReadLine().ToLower();
+
+if (username == "kalleanka")
+{
+    Console.WriteLine("Welcome!");
+}
+else 
+{
+    Console.WriteLine("Wrong username.");
+}
+
+// Console.WriteLine("Enter username:");
+// String username2 = Console.ReadLine();
+Console.WriteLine("Enter password:");
+String password = Console.ReadLine();
+
+if ( username == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Welcome!");  
+}
+else
+{
+    Console.WriteLine("Wrong username and wrong password");
+}
+
+
+for (int i = 0; i < 32; i++)
+{
+    Console.WriteLine("Hello, World");
+}
+
+while (password != )
+
+
+Console.ReadLine();
+
+
+
+
+
+
